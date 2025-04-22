1426d52d0e8610aa3bd0f8175a9f8ce8baa0fcc5
tizianotambone <tambonetiziano@gmail.com>
2025-04-22 14:22:04 +0200

    [UPD] definizione crud

---
+++ controllers/controllers.js
@@ -0,0 +1,25 @@
+const posts=require(`../data/posts.js`)
+
+
+function index(req,res){
+    
+
+}
+function show(req,res){
+
+}
+
+function store(req,res){
+
+}
+
+function update(req,res){
+
+}
+function modify(req,res){
+
+}
+
+function destroy(req,res){
+    
+}
