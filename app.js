0eeadb2b01df5b38d293d8d247070d94fc844fd7
tizianotambone <tambonetiziano@gmail.com>
2025-04-23 14:33:42 +0200

    [UPD] indico ad express di trattare i body in json

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const blogrouters = require('./routers/posts.js')
+// indico ad express di trattare i body in forma json
+app.use(express.json()) 
 
 app.use('/blog', blogrouters);
 console.log(blogrouters)
